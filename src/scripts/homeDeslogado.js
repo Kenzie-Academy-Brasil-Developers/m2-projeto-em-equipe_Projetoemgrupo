fe3c61e78d878f963df012d460ac6981659e4fdd
Gedson <gedsoncandido94@gmail.com>
2023-01-16 22:15:45 -0300

    feat:cadastro

---
+++ src/scripts/homeDeslogado.js
@@ -0,0 +1,40 @@
+import { createUser } from "./requests.js"
+
+function openModalCadastro(){
+    const buttonRegister = document.querySelector('.Botao_Register')
+    const modal = document.querySelector('.Modal_Cadastro')
+    const buttonCloseModal = document.querySelector('.Fecha_Modal')
+
+    buttonRegister.addEventListener('click', () =>{
+        modal.showModal()
+    })
+
+    buttonCloseModal.addEventListener('click', () =>{
+        modal.showModal()
+    })
+}
+
+openModalCadastro()
+
+function createUserForm(){
+    const inputs = document.querySelectorAll('.Form_Cadastro > input')
+    const button = document.querySelector('.Cadastrar')
+    
+    const newUser = {}
+
+    button.addEventListener('click', async (event) => {
+        event.preventDefault()
+        
+        inputs.forEach(input => {
+            newUser[input.name] = input.value
+        })
+
+    
+       const request = await createUser(newUser)
+       localStorage.setItem('@kenziePet:cadastro', JSON.stringify(request))
+    })
+    
+    return newUser
+}
+
+createUserForm()
--- src/scripts/requests.js
+++ src/scripts/requests.js
@@ -1,1 +1,25 @@
 /* A cada Requisição coloque um comentário informando quem fez*/
+
+//Gedson
+export async function createUser(data){
+    const creatUserData = await fetch(`http://localhost:3333/users`,{
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+
+    const creatUserDataJson = await creatUserData.json()
+
+    if(!creatUserData.ok){
+        alert(`${creatUserDataJson.message}`)
+        window.location.replace("./homeDeslogado.html")
+        
+    }
+    else{
+        alert(`Cadastro realizado com sucesso`)
+        window.location.replace("./homeDeslogado.html")
+    }
+    return creatUserDataJson
+}
