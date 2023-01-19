5eee5c7408c5f2f0222476af8ab47b59e5dc79de
Gabriel Montenegro <itsgabrielmontenegro@gmail.com>
2023-01-19 19:03:03 -0300

    atualização erros registro, login e cadastrar pets

--- src/scripts/requests.js
+++ src/scripts/requests.js
@@ -30,43 +30,36 @@ export async function loginUser(dataLogin) {
             body: JSON.stringify(dataLogin),
         });
         let requestJson = await request.json();
-            console.log(requestJson)
+        // console.log(requestJson);
         if (requestJson.token) {
             localStorage.setItem("token", JSON.stringify(requestJson.token));
             // console.log("logou");
 
             window.location.replace("./src/pages/homeLogado.html");
             return requestJson;
-
         } else {
             console.log(requestJson.message);
         }
     } catch (err) {
         console.log(err);
     }
-    
 }
 
-//Diogo 
+//Diogo
 
-import{renderProfile} from './perfil.js'
+// import { renderProfile } from "./perfil.js";
 
-export async function ProfilePage(){
-    const tokenJson = localStorage.getItem("token")
-    const token = JSON.parse(tokenJson)
-     
-      const request = await fetch('http://localhost:3333/users/profile',{
-            method:"GET",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization:`Bearer ${token}`
-            }
-        })
-        const requestJSON =  await request.json()
-            renderProfile(requestJSON)
+// export async function ProfilePage() {
+//     const tokenJson = localStorage.getItem("token");
+//     const token = JSON.parse(tokenJson);
 
-        }
-    
-    
-
-
+//     const request = await fetch("http://localhost:3333/users/profile", {
+//         method: "GET",
+//         headers: {
+//             "Content-Type": "application/json",
+//             Authorization: `Bearer ${token}`,
+//         },
+//     });
+//     const requestJSON = await request.json();
+//     renderProfile(requestJSON);
+// }
